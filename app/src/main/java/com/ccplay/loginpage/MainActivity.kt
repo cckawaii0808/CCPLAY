36d788f443770ee53e3afcc16266e50e4fc8a490
cc <cc_xie@gmail.com>
2022-03-08 17:43:33 +0800

    初始版本－設定好背景

--- app/src/main/java/com/ccplay/loginpage/MainActivity.kt
+++ app/src/main/java/com/ccplay/loginpage/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ccplay.loginpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
